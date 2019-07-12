f3eccf7729ad52b5818cfdfd9322c382e4a12088
Ryan Nehring <rnehring@PUREFLEX.COM>
2019-07-12 11:03:57 -0400

    More Fixes

--- src/components/Highlights.vue
+++ src/components/Highlights.vue
@@ -22,6 +22,6 @@ export default {
     "uv-index": UVIndex,
     "visibility": Visibility,
     "wind-status": WindStatus,
-  },
+  }
 };
 </script>
--- src/components/TempVarChart.vue
+++ src/components/TempVarChart.vue
@@ -75,7 +75,7 @@ export default {
      },
      deep: true
    },
- },
+ }
 };
 </script>
 
